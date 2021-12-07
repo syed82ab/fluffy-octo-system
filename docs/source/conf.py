8e02cf1aef0e62dfe11b3a53566ab40004c172d8
syed82ab <syed.aljunid@gmail.com>
2021-12-07 09:07:36 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,8 @@ version = '0.1.0'
 # -- General configuration
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../../rest/app/models/'))
 sys.path.insert(0, os.path.abspath('../../lumache/'))
-sys.path.append(os.path.abspath('../../rest/app/models/'))
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
